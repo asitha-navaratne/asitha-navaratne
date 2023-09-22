531873eae95853bd80344b6403dd400a7a1da8ba
Asitha Navaratne <asithan476@gmail.com>
2023-09-22 09:12:48 +0530

    Fixed source map loading warning on console

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+      {
         test: /\.(scss)$/,
         use: [
           {
